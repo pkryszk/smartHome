bfbda0c7cedaaabf259a54e17119886392cde7b6
pkryszk <pkryszk-github@yahoo.com>
2023-04-25 20:04:22 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def main():
+# TODO  tworzy instancję IOTService
+# TODO tworzy instancje dostępnych urządzeń
+# TODO rejestruje urządzenia
+# TODO testuje urządzenia
+# TODO tworzy programy (listy Message)
+# np program wake up
+# włącza światłą (MessageType.SWITCH_ON)
+# włącza głośniki
+# odgrywa piosenkę (MessageType.PLAY_SONG)
+# otwiera zasłony
+#
+# program sleep
+# wyłącza światła ( MessageType.SWITCH_OFF)
+# wyłącza głośniki
+# zasłania zasłony (MessageType.CLOSE)
+#
+# TODO uruchamia programy
+# TODO usuwa urządzenia
+# TODO konczy program
+  pass
+
+if __name__ == '__main__':
+    main()
+
