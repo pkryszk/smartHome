98bb54eca01d39351cf9da372128bf1c0f2f4139
pkryszk <pkryszk-github@yahoo.com>
2023-04-25 20:26:29 +0200

    copy of iot/message.py from homework instruction

--- iot/message.py
+++ iot/message.py
@@ -1,1 +1,18 @@
-# TODO add logic
+from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class MessageType(Enum):
+    SWITCH_ON = auto()
+    SWITCH_OFF = auto()
+    CHANGE_COLOR = auto()
+    PLAY_SONG = auto()
+    OPEN = auto()
+    CLOSE = auto()
+
+
+@dataclass
+class Message:
+    device_id: str
+    msg_type: MessageType
+    data: str = ""
